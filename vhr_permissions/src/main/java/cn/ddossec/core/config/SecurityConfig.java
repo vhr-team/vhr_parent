d31e6d59a67861074da3b65582902cc206781fdd
xhh <303158131@qq.com>
2020-04-12 22:19:45 +0800

    没有登录，最近访问接口的异常处理，重定向跨域处理

--- vhr_permissions/src/main/java/cn/ddossec/core/config/SecurityConfig.java
+++ vhr_permissions/src/main/java/cn/ddossec/core/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -164,6 +165,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .permitAll()
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                // 没有登录，没有认证，没有权限，不要重定向，异常处理
+                .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
+                resp.setContentType("application/json;charset=utf-8");
+                PrintWriter out = resp.getWriter();
+
+                ResponseResult error = ResponseResult.error("访问失败!");
+                if (e instanceof InsufficientAuthenticationException) {
+                    error = ResponseResult.error("请求失败,请联系管理员!");
+                }
+                String res = new ObjectMapper().writeValueAsString(error);
+                out.write(res);
+                out.flush();
+                out.close();
+            }
+        });
     }
 }
